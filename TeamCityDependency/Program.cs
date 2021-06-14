a578813bb2d4a424210c21076151c629b0496e6c
Khushal Pujara <84337247+datacore-kpujara@users.noreply.github.com>
2021-06-14 16:59:40 -0400

    Trying to output artifact

--- TeamCityDependency/Program.cs
+++ TeamCityDependency/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TeamCityDependency
 {
@@ -6,7 +7,13 @@ namespace TeamCityDependency
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines =
+            {
+                "First line", "Second line", "Third line"
+            };
+
+            File.WriteAllLinesAsync("WriteLines.txt", lines);
+
         }
     }
 }
