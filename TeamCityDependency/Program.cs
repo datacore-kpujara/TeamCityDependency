aeb1aaed834721f5201be20b4216a7ea60720491
Khushal Pujara <84337247+datacore-kpujara@users.noreply.github.com>
2021-06-15 10:29:07 -0400

    Added webrequest in program

--- TeamCityDependency/Program.cs
+++ TeamCityDependency/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
 
 namespace TeamCityDependency
 {
@@ -7,12 +9,32 @@ namespace TeamCityDependency
     {
         static void Main(string[] args)
         {
-            string[] lines =
+            /*string[] lines =
             {
                 "First line", "Second line", "Third line"
             };
 
-            File.WriteAllLinesAsync("WriteLines.txt", lines);
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);*/
+
+           
+
+            List<string> list = new List<string>();
+
+            var webRequest = WebRequest.Create(@"http://172.20.0.179/httpAuth/downloadBuildLog.html?buildId=324080");
+            webRequest.Method = "GET";
+            webRequest.Headers["Authorization"] = "Basic " + Convert.ToBase64String(System.Text.Encoding.Default.GetBytes("kpujara:Lilyaldrin123"));
+
+            using (var response = webRequest.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    list.Add(reader.ReadLine());
+                }
+            }
+
+            File.WriteAllLinesAsync(AppDomain.CurrentDomain.BaseDirectory + "\\WriteLines.txt", list);
 
         }
     }
